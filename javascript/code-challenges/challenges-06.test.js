eac571212c1429e5adcfec51568e573e207b3588
Joel Connell <joel.connell@yahoo.com>
2021-06-15 16:10:40 -0700

    challenge 7

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -92,7 +92,6 @@ const addBirthYearProperty = (obj, year) => {
   // Solution code here...
   obj.yearBorn=year;
 };
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5 - Stretch Goal
 
