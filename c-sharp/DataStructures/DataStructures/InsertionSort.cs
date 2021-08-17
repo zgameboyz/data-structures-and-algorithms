2aa143013618f548905080fe95e9b2e6efa01764
Joel <joel.connell@yahoo.com>
2021-08-16 18:43:37 -0700

    Added Code Challenge 26

---
+++ c-sharp/DataStructures/DataStructures/InsertionSort.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+ public class InsertionSort
+  {
+
+    
+     
+
+       
+
+        
+
+  
+
+      public static int[] InsertionSortArray(int[] arr)
+      {
+        for (int i = 1; i < arr.Length; i++)
+
+        {
+
+          int j = i - 1;
+
+          int temp = arr[i];
+
+          while (j >= 0 && temp < arr[j])
+          {
+
+            arr[j + 1] = arr[j];
+
+            j = j - 1;
+          }
+
+          arr[j + 1] = temp;
+        }
+      return arr;
+      }
+    }
+
+
+
+
+  
+}
---
+++ c-sharp/images/DataStructures/DataStructureTests/LevelOrderTraversalTest.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using DataStructures;
+using Xunit;
+
+namespace DataStructuresTest
+{
+  public class LevelOrderTraversalTest : BinaryTree<int>
+  {
+    [Fact]
+    public void LevelOrderTraversalTesting()
+    {
+      BinaryTree<int> tree = new BinaryTree<int>();
+
+     
+
+      Node<int> One = new Node<int>(2);
+      Node<int> Two = new Node<int>(7);
+      Node<int> Three = new Node<int>(5);
+      Node<int> Four = new Node<int>(2);
+      Node<int> Five = new Node<int>(6);
+      Node<int> Six = new Node<int>(9);
+      Node<int> Seven = new Node<int>(5);
+      Node<int> Eight = new Node<int>(11);
+      Node<int> Nine = new Node<int>(4);
+
+      tree.Root = One;
+      One.Left = Two;
+      One.Right = Three;
+      Two.Left = Four;
+      Two.Right = Five;
+      Three.Right = Six;
+      Five.Left = Seven;
+      Five.Right = Eight;
+      Six.Left = Nine;
+
+      List<int> list = LevelOrderTraversal(tree);
+      foreach (int num in list)
+      {
+        Console.Write($"{num} ");
+      }
+
+
+
+      Assert.Equal(list, LevelOrderTraversal(tree));
+
+
+    }
+
+  }
+}
