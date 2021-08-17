bf5ff4d9939cde7011a43066fd89e85a4976caf1
Joel <joel.connell@yahoo.com>
2021-08-16 18:46:18 -0700

    Revert "Added Code Challenge 26"
    This reverts commit 2aa143013618f548905080fe95e9b2e6efa01764.

--- c-sharp/DataStructures/DataStructures/Program.cs
+++ c-sharp/DataStructures/DataStructures/Program.cs
@@ -3,43 +3,35 @@ using System.Collections.Generic;
 
 namespace DataStructures
 {
-  class Program : InsertionSort
+  class Program : BinaryTree<int>
   {
     static void Main(string[] args)
     {
-      int[] arr = { 2, 6, 8, 7, 1 };
-      int[] sorted = InsertionSortArray(arr);
-      for (int i =0; i <arr.Length; i++) {
+      BinaryTree<int> tree = new BinaryTree<int>();
 
-
-        Console.WriteLine(sorted[i]);
-
-      }
-      //BinaryTree<int> tree = new BinaryTree<int>();
-
-      //Node<int> One = new Node<int>(2);
-      //Node<int> Two = new Node<int>(7);
-      //Node<int> Three = new Node<int>(5);
-      //Node<int> Four = new Node<int>(2);
-      //Node<int> Five = new Node<int>(6);
-      //Node<int> Six = new Node<int>(9);
-      //Node<int> Seven = new Node<int>(5);
-      //Node<int> Eight = new Node<int>(11);
-      //Node<int> Nine = new Node<int>(4);
-      //tree.Root = One;
-      //One.Left = Two;
-      //One.Right = Three;
-      //Two.Left = Four;
-      //Two.Right = Five;
-      //Three.Right = Six;
-      //Five.Left = Seven;
-      //Five.Right = Eight;
-      //Six.Left = Nine;
-
-      //List<int> list = LevelOrderTraversal(tree);
-      //foreach (int num in list)
+      Node<int> One = new Node<int>(2);
+      Node<int> Two = new Node<int>(7);
+      Node<int> Three = new Node<int>(5);
+      Node<int> Four = new Node<int>(2);
+      Node<int> Five = new Node<int>(6);
+      Node<int> Six = new Node<int>(9);
+      Node<int> Seven = new Node<int>(5);
+      Node<int> Eight = new Node<int>(11);
+      Node<int> Nine = new Node<int>(4);
+      tree.Root = One;
+      One.Left = Two;
+      One.Right = Three;
+      Two.Left = Four;
+      Two.Right = Five;
+      Three.Right = Six;
+      Five.Left = Seven;
+      Five.Right = Eight;
+      Six.Left = Nine;
+      
+      List<int> list = LevelOrderTraversal(tree);
+      foreach (int num in list)
       {
-        //Console.Write($"{num} ");
+        Console.Write($"{num} ");
       }
     }
   }
